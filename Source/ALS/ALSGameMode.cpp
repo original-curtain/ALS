c70111d35b635822c5383b73848dda7d099f1776
daniel <1214626032@qq.com>
2022-07-07 21:51:16 +0800

    fix

--- Source/ALS/ALSGameMode.cpp
+++ Source/ALS/ALSGameMode.cpp
@@ -7,9 +7,9 @@
 AALSGameMode::AALSGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	}*/
 }
